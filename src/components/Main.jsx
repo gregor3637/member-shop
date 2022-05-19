00651144d33d9dfe5b3f46645fd84de8d8a0e540
mihailm956 <mihailm956@gmail.com>
2022-05-19 12:59:50 +0300

    new page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,7 @@ import ItemPage2 from "./pages/ItemPage/ItemPage2";
 import TrendingPage from "./pages/TrendingPage/TrendingPage";
 import CollectionsPage from "./pages/CollectionsPage/CollectionsPage";
 import ActivityPage from "./pages/ActivityPage/ActivityPage";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
 
 const MarketplacePage = React.lazy(() =>
   import("./pages/MarketplacePage/MarketplacePage")
@@ -54,6 +55,10 @@ const Main = (props) => {
 
           <Route path="/marketplace">
             <MarketplacePage />
+          </Route>
+
+          <Route path="/accout/settings">
+            <SettingsPage />
           </Route>
 
           <Route path="*">
